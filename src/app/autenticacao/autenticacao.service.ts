cab4132060839ba5b5330f9eba10563f4aa1cef9
Heloisahelodahlke@hotmail.com <helodahlke@hotmail.com>
2022-10-03 22:47:25 -0300

    Criando serviço de token

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,19 +1,26 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http'
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root' //esse serviço quando instanciado, o angular insttancia uma vez só (singleton)
 })
 export class AutenticacaoService {
 
-  constructor(private httpCliente: HttpClient) { }
+  constructor(private httpCliente: HttpClient, private usuarioSerivce: UsuarioService) { }
 
-  autenticar(usuario: string, senha: string): Observable<any> {
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
     return this.httpCliente.post('http://localhost:3000/user/login', {
       userName: usuario,
       password: senha,
-    });
+    },
+    { observe: 'response' }
+    ).pipe(tap((res) => {
+      const authToken = res.headers.get('x-access-token') ?? '';
+      this.usuarioSerivce.salvaToken(authToken);
+    })
+    ); //pipe: para fazer operações (salvar/alterar)
   }
 
 
